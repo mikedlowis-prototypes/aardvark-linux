5380e207f8c8e3eadadb68ccd0ea6f698a2cb985
Mike Lowis <mike.lowis@gentex.com>
2016-01-27 14:25:17 +0000

    Added a few more gnu packages to round out the base chroot system. Also install the sources for all  base packages into /src

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ gitclone(){
 fetch crossx86-x86_64-linux-musl-1.1.12.tar.xz \
       https://e82b27f594c813a5a4ea5b07b06f16c3777c3b8c.googledrive.com/host/0BwnS5DMB0YQ6bDhPZkpOYVFhbk0/musl-1.1.12/ \
       "$AL_TOOLS"
+rm -f "$AL_TOOLS/$AL_TGT/lib/libc.so"
 
 # Install sbase
 gitclone http://git.suckless.org/sbase "$AL_SOURCES/sbase"
@@ -98,7 +99,8 @@ if [ ! -f "$AL_ROOT/bin/gawk" ]; then
         --disable-nls        \
         --without-readline
     make $MAKEFLAGS gawk
-    cp "$AL_SOURCES/gawk/gawk" "$AL_ROOT/bin"
+    cp "$AL_SOURCES/gawk/gawk" "$AL_ROOT/bin/gawk"
+    ln -sfv gawk "$AL_ROOT/bin/awk"
     cd $AL
 fi
 
@@ -114,12 +116,69 @@ if [ ! -f "$AL_ROOT/bin/make" ]; then
     cd $AL
 fi
 
+## Install GNU bc
+#fetch bc-1.06.tar.gz http://ftp.gnu.org/gnu/bc/ "$AL_SOURCES/bc"
+#if [ ! -f "$AL_ROOT/bin/bc" ]; then
+#    cd "$AL_SOURCES/bc"
+#    ./configure              \
+#        LDFLAGS="--static"   \
+#        --prefix="$AL_ROOT"
+#    make $MAKEFLAGS install
+#    cd $AL
+#fi
+#
+## Install GNU gzip
+#fetch gzip-1.6.tar.xz http://ftp.gnu.org/gnu/gzip/ "$AL_SOURCES/gzip"
+#if [ ! -f "$AL_ROOT/bin/gzip" ]; then
+#    cd "$AL_SOURCES/gzip"
+#    ./configure              \
+#        LDFLAGS="--static"   \
+#        --prefix="$AL_ROOT"
+#    make $MAKEFLAGS install
+#    cd $AL
+#fi
+#
+## Install GNU ncurses
+#fetch ncurses-6.0.tar.gz http://ftp.gnu.org/gnu/ncurses/ "$AL_SOURCES/ncurses"
+#if [ ! -f "$AL_ROOT/lib/ncurses" ]; then
+#    cd "$AL_SOURCES/ncurses"
+#    ./configure              \
+#        LDFLAGS="--static"   \
+#        --prefix="$AL_TOOLS/lib/gcc/$AL_TGT/5.3.0"  \
+#        --without-shared     \
+#        --without-debug      \
+#        --without-ada        \
+#        --enable-widec       \
+#        --enable-overwrite
+#    make $MAKEFLAGS install
+#    cd $AL
+#fi
+#/tools/bin/../lib/gcc/x86_64-linux-musl/5.3.0/include
+
+## Install Perl
+#fetch perl-5.22.0.tar.bz2 http://www.cpan.org/src/5.0/ "$AL_SOURCES/perl"
+#if [ ! -f "$AL_ROOT/bin/perl" ]; then
+#    cd "$AL_SOURCES/perl"
+#    ./Configure -des -Dprefix="$AL_ROOT"
+#    make $MAKEFLAGS
+#    cd $AL
+#fi
+
 ###############################################################################
 # Install Sources
 ###############################################################################
-fetch linux-4.4.tar.xz https://cdn.kernel.org/pub/linux/kernel/v4.x/ "$AL_ROOT/src/linux"
-#fetch make-4.1.tar.gz http://ftp.gnu.org/gnu/make/ "$AL_ROOT/src/make"
-#fetch musl-1.1.12.tar.gz http://www.musl-libc.org/releases/ "$AL_SOURCES/musl"
+fetch    musl-1.1.12.tar.gz  http://www.musl-libc.org/releases/            "$AL_ROOT/src/musl"
+gitclone                     http://git.suckless.org/sbase                 "$AL_ROOT/src/sbase"
+gitclone                     http://git.suckless.org/ubase                 "$AL_ROOT/src/ubase"
+fetch    mksh-R52b.tgz       https://www.mirbsd.org/MirOS/dist/mir/mksh/   "$AL_ROOT/src/mksh"
+fetch    make-4.1.tar.gz     http://ftp.gnu.org/gnu/make/                  "$AL_ROOT/src/make"
+fetch    grep-2.9.tar.xz     http://ftp.gnu.org/gnu/grep/                  "$AL_ROOT/src/grep"
+fetch    gawk-4.1.3.tar.xz   http://ftp.gnu.org/gnu/gawk/                  "$AL_ROOT/src/gawk"
+#fetch    bc-1.06.tar.gz      http://ftp.gnu.org/gnu/bc/                    "$AL_ROOT/src/bc"
+#fetch    gzip-1.6.tar.xz     http://ftp.gnu.org/gnu/gzip/                  "$AL_ROOT/src/gzip"
+#fetch    ncurses-6.0.tar.gz  http://ftp.gnu.org/gnu/ncurses/               "$AL_ROOT/src/ncurses"
+#fetch    perl-5.22.0.tar.bz2 http://www.cpan.org/src/5.0/                  "$AL_ROOT/src/perl"
+#fetch    linux-4.4.tar.xz    https://cdn.kernel.org/pub/linux/kernel/v4.x/ "$AL_ROOT/src/linux"
 
 ###############################################################################
 # Finalize the Chroot
@@ -129,3 +188,30 @@ mkdir -pv "$AL_ROOT/proc"
 mkdir -pv "$AL_ROOT/sys"
 mkdir -pv "$AL_ROOT/tmp"
 mkdir -pv "$AL_ROOT/root"
+ln -sfv "$AL_TGT-addr2line"  "$AL_TOOLS/bin/addr2line"
+ln -sfv "$AL_TGT-ar"         "$AL_TOOLS/bin/ar"
+ln -sfv "$AL_TGT-as"         "$AL_TOOLS/bin/as"
+ln -sfv "$AL_TGT-c++"        "$AL_TOOLS/bin/c++"
+ln -sfv "$AL_TGT-c++filt"    "$AL_TOOLS/bin/c++filt"
+ln -sfv "$AL_TGT-cpp"        "$AL_TOOLS/bin/cpp"
+ln -sfv "$AL_TGT-elfedit"    "$AL_TOOLS/bin/elfedit"
+ln -sfv "$AL_TGT-g++"        "$AL_TOOLS/bin/g++"
+ln -sfv "$AL_TGT-gcc"        "$AL_TOOLS/bin/gcc"
+ln -sfv "$AL_TGT-gcc-5.3.0"  "$AL_TOOLS/bin/gcc-5.3.0"
+ln -sfv "$AL_TGT-gcc-ar"     "$AL_TOOLS/bin/gcc-ar"
+ln -sfv "$AL_TGT-gcc-nm"     "$AL_TOOLS/bin/gcc-nm"
+ln -sfv "$AL_TGT-gcc-ranlib" "$AL_TOOLS/bin/gcc-ranlib"
+ln -sfv "$AL_TGT-gcov"       "$AL_TOOLS/bin/gcov"
+ln -sfv "$AL_TGT-gcov-tool"  "$AL_TOOLS/bin/gcov-tool"
+ln -sfv "$AL_TGT-gprof"      "$AL_TOOLS/bin/gprof"
+ln -sfv "$AL_TGT-ld"         "$AL_TOOLS/bin/ld"
+ln -sfv "$AL_TGT-ld.bfd"     "$AL_TOOLS/bin/ld.bfd"
+ln -sfv "$AL_TGT-nm"         "$AL_TOOLS/bin/nm"
+ln -sfv "$AL_TGT-objcopy"    "$AL_TOOLS/bin/objcopy"
+ln -sfv "$AL_TGT-objdump"    "$AL_TOOLS/bin/objdump"
+ln -sfv "$AL_TGT-ranlib"     "$AL_TOOLS/bin/ranlib"
+ln -sfv "$AL_TGT-readelf"    "$AL_TOOLS/bin/readelf"
+ln -sfv "$AL_TGT-size"       "$AL_TOOLS/bin/size"
+ln -sfv "$AL_TGT-strings"    "$AL_TOOLS/bin/strings"
+ln -sfv "$AL_TGT-strip"      "$AL_TOOLS/bin/strip"
+
